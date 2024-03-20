2869fe01488406d4ba81123dc8a1d9abcb6199c0
Adrian Antonio Auqui Perez <adrian.auqui@utec.edu.pe>
2024-03-20 01:53:44 -0500

    About Us update 1

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,59 +1,89 @@
 import { CachimboLogo } from "@/components/icons/CachimoLogo";
 import { Link } from "react-router-dom";
+import Antonio from "@/assets/TeamPhoto/Antonio.jpg";
+import Yanelit from "@/assets/TeamPhoto/Yanelit.jpg";
+import Brigit from "@/assets/TeamPhoto/Brigit.jpeg";
+import Adrian from "@/assets/TeamPhoto/Adrian.jpeg";
+import Noe from "@/assets/TeamPhoto/Noe.jpeg";
+import Danilo from "@/assets/TeamPhoto/Danilo.jpeg";
+
 
 const AboutUs = () => {
   const Team = [
     {
-      name: "Adrian Antonio",
-      lastName: "Auqui Perez",
+      name: "Adrian",
+      lastName: "Antonio Auqui Perez",
       email: "adrian.auqui@utec.edu.pe",
-      description: "Bla bla",
-      image: "./assets/auqui.png",
+      rol: "Presidente",
+      image: Antonio,
     },
-    // mas integrantes
-    // ...
-    // ...
+    {
+      name: "Yanelit",
+      lastName: "Picon Huaman",
+      email: "yanelit.huaman@utec.edu.pe",
+      rol: "Secretaria",
+      image: Yanelit,
+    },
+    {
+      name: "Brigitte Dayane",
+      lastName: "Rojas Leon",
+      email: "brigitte.rojas@utec.edu.pe",
+      rol: "Coordinadora Recursos Educativos",
+      image: Brigit,
+    },
+    {
+      name: "Adrian",
+      lastName: "Sandoval Huamani",
+      email: "adrian.sandoval@utec.edu.pe",
+      rol: "Backend Developerr",
+      image: Adrian,
+    },
+    {
+      name: "Noe",
+      lastName: "Paredes Rauraico",
+      email: "ananias.paredes@utec.edu.pe",
+      rol: "Web Designer",
+      image: Noe,
+    },
+    {
+      name: "Danilo Caleb",
+      lastName: "Becerra Balvin",
+      email: "danilo.becerra@utec.edu.pe",
+      rol: "Tesorero",
+      image: Danilo,
+    },
+    // Más integrantes...
   ];
 
   return (
-    <main className="m-auto flex min-h-screen w-[500px] flex-col">
-      <div className="flex justify-center pb-10 pt-32">
+    <main className="m-auto flex flex-col items-center min-h-screen px-4">
+      <div className="py-10 pt-20">
         <CachimboLogo />
       </div>
-      <section className="flex flex-col items-center px-4">
-        <div className="pb-20 text-3xl font-bold text-cach-l2">
-          Nuestro equipo
-        </div>
-        <div className="py-30 mb-10 flex flex-row items-center justify-center rounded-xl border border-solid border-cach-l3 bg-cach-l3 px-6 dark:border-cach-l2 dark:bg-transparent">
-          <ul className="text-cach-l2">
-            {Team.map((member) => (
-              <li key={member.name}>
-                <article className="flex flex-row items-center justify-between gap-4">
-                  <div className="h-full w-full">
-                    <img
-                      className="m-4 aspect-[12/9] size-[150px] object-fill"
-                      src={member.image}
-                    ></img>
-                  </div>
-                  <div className="h-full w-3/4 space-y-4 px-8 py-6">
-                    <h1 className="text-2xl">
-                      {member.name} {member.lastName}
-                    </h1>
-                    <h2>{member.email}</h2>
-                    <p className="text-pretty">{member.description}</p>
-                  </div>
-                </article>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <section className="w-full max-w-lg grid grid-cols-1 md:grid-cols-2 gap-6">
+        {Team.map((member, index) => (
+          <div key={index} className="bg-cach-l3 rounded-xl border border-solid border-cach-l3 dark:border-cach-l2 dark:bg-transparent p-2">
+            <img
+              className="w-full h-64 object-cover rounded-t-xl"
+              src={member.image}
+              alt={`${member.name} ${member.lastName}`}
+            />
+            <div className="p-4">
+              <h1 className="text-xl font-semibold">{member.name} {member.lastName}</h1>
+              <h2 className="text-sm text-gray-600">{member.email}</h2>
+              <p className="text-sm text-pretty">{member.rol}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+      <div className="py-6">
         <Link
           to="/home"
-          className="w-30 mb-4 rounded-full bg-cach-l3 px-8 py-3 text-center font-semibold text-white duration-300 hover:bg-cach-l2"
+          className="w-full rounded-full bg-cach-l3 px-8 py-3 text-center font-semibold text-white duration-300 hover:bg-cach-l2"
         >
           Ir atrás
         </Link>
-      </section>
+      </div>
     </main>
   );
 };
