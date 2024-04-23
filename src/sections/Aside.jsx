56f051f20a933a135d16b2ce49440b6cb5901498
JosephAnderson234 <coserojasj@gmail.com>
2024-04-22 22:02:11 -0500

    On branch sideAvance Changes to be committed:     modified:   package.json 	modified:   src/sections/Aside.jsx
    Changes not staged for commit:
    	modified:   .gitignore
    	modified:   src/sections/Aside.jsx
    
    Untracked files:
    	src/assets/profile-template.png

--- src/sections/Aside.jsx
+++ src/sections/Aside.jsx
@@ -1,12 +1,19 @@
+import { CachimboLogo } from "../components/icons/CachimoLogo";
+import ThemeButton from "../components/ThemeButton";
+
 const Aside = () => {
   return (
     <aside className="min-h-screen min-w-[20%] border border-black p-4">
       <div className="h-full">
         {/* logo cachimbo */}
+        <CachimboLogo className="mb-4 lg:mb-0" />
         Aside y opciones
         <div>
           {/* componente cuadro de perfil */}
           {/* datos de perfil */}
+          <div className="flex justify-center">
+            <ThemeButton />
+          </div>
         </div>
         {/* componente de section buttons */}
       </div>
