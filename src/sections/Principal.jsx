5cb41ef1bb48ba47c2380344521089ef2f2835ce
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2024-04-22 19:28:17 -0500

    Made structure columnas for work

--- src/sections/Principal.jsx
+++ src/sections/Principal.jsx
@@ -1,5 +1,5 @@
 const Principal = () => {
-  return <div className="h-full w-0 overflow-y-scroll">Principal</div>;
+  return <div className="h-full overflow-y-scroll">Principal</div>;
 };
 
 export default Principal;
