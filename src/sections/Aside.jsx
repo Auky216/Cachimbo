90d22c9e1b440b0f7ca6d8eba7d87dc8e5ef0000
JosephAnderson234 <coserojasj@gmail.com>
2024-04-22 22:04:20 -0500

    startingCommit

--- src/sections/Aside.jsx
+++ src/sections/Aside.jsx
@@ -1,5 +1,6 @@
 import { CachimboLogo } from "../components/icons/CachimoLogo";
 import ThemeButton from "../components/ThemeButton";
+import profileTemplate from "../assets/profile-template.png";
 
 const Aside = () => {
   return (
@@ -7,15 +8,15 @@ const Aside = () => {
       <div className="h-full">
         {/* logo cachimbo */}
         <CachimboLogo className="mb-4 lg:mb-0" />
-        Aside y opciones
         <div>
           {/* componente cuadro de perfil */}
           {/* datos de perfil */}
-          <div className="flex justify-center">
-            <ThemeButton />
-          </div>
         </div>
         {/* componente de section buttons */}
+      
+        <div className="flex justify-center">
+            <ThemeButton />
+        </div>
       </div>
     </aside>
   );
