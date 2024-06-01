f10e4662b3a6c006c90aa78148c4169e80178021
rominaromani <mariajulia.romani@utec.edu.pe>
2024-06-01 00:56:31 -0500

    pdfs file as a temporarily created constant

---
+++ src/constant/pdfs.js
@@ -0,0 +1,32 @@
+export const getPdfs = {
+    calculo1: {
+        title: "Cálculo de una Variable 1 - PC1 2021-2",
+        autor: "Jojiz",
+        likes: 10,
+        descripcion: "Parcial de Cálculo de una Variable 1, \
+                      2021-2, con soluciones detalladas.",
+        
+    },
+    machineLearning: {
+        title: "Machine Learning - Kmeans",
+        autor: "Tarik",
+        likes: 0,
+        descripcion: "Introducción al Machine Learning, \
+                      con ejemplos de Kmeans.",
+    },
+    ecDiferenciales: {
+        title: "Ecuaciones Diferenciales - Parcial Final 2023-2",
+        autor: "PRC",
+        likes: 55,
+        descripcion: "Parcial y final de Ecuaciones Diferenciales, \
+                      2023-2, con soluciones detalladas.",
+    },
+    Progra2: {
+        title: "Programación 2 - Anotaciones",
+        autor: "holamundo123",
+        likes: 3,
+        descripcion: "Anotaciones de Programación 2, \
+                      con ejemplos de código.",
+    },
+
+}
