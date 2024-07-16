666f8fe100da0765de8946eea9cdb86694ee39ee
JosephAnderson234 <joseph.cose@utec.edu.pe>
2024-07-16 09:55:40 -0500

    Little changes in constants and styles

--- src/constant/teachers.js
+++ src/constant/teachers.js
@@ -13,14 +13,14 @@ export const teachers = [
     id: 1,
     name: "Patricia Reynoso Quispe",
     photo: "https://utec.edu.pe/sites/default/files/pictures/picture-1031-1621028130.png",
-    courses: ["calcvar"],  //["calcvar", "calcvect", "edo"],
+    courses: ["calcvar", "calcvect"],  //["calcvar", "calcvect", "edo"],
     rate: 3,
     descrip: "El profesor más amado por los estudiantes de UTEC.",
   },
   {
     id: 2,
     name: "Henry Gallegos",
-    photo: "",
+    photo: "https://cdn.inst-fs-iad-prod.inscloudgate.net/abf6fa21-9936-4994-bd8c-a01c9bdb9be8?token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6ImNkbiJ9.eyJyZXNvdXJjZSI6Ii9hYmY2ZmEyMS05OTM2LTQ5OTQtYmQ4Yy1hMDFjOWJkYjliZTgiLCJ0ZW5hbnQiOiJjYW52YXMiLCJ1c2VyX2lkIjoiNDY4OTAwMDAwMDAwMzEzMjgiLCJpYXQiOjE3MjExMDAyNTcsImV4cCI6MTcyMTE4NjY1N30.bMAbCZC956F3629rHQJadI8wr-UrWob1HChtevGhN3ZDhL8OPJSFQ7VZlEQzoUNjrdVK_KVgRyFnr-UJDTy5hg&content_type=image%2Fpng",
     courses: ["progra1", "progra2"],
     rate: 4,
     descrip: "El profesor mas tranquilo de la Utec",
@@ -28,10 +28,18 @@ export const teachers = [
   {
     id: 276,
     name: "Zorrilla",
-    photo: "",
+    photo: "https://utec.edu.pe/sites/default/files/pictures/picture-651-1565988664.png",
     courses: ["labCom1"],
     rate: 4,
     descrip: "Sus coordinadores dan miedo",
+  },
+  {
+    id: 250,
+    name: "Julio Gutierrez",
+    photo: "https://utec.edu.pe/sites/default/files/pictures/picture-1176-1666217314.png",
+    courses: ["matedisc1"],
+    rate: 5,
+    descrip: "Buen profesor",
   }
 ];
 
