48966b9e7fe461ac1c83433d8060c4e5eba49372
Fabrizzio Vilchez <115495332+Fabrizzio20k@users.noreply.github.com>
2024-08-15 12:45:31 -0500

    Modified files

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -65,19 +65,22 @@ const Login = () => {
 
     return (
         // DIV para ajustar el centrado con todo el h-screen del #App
-        <div className="m-auto flex min-h-screen w-[500px] items-center">
-            <section className="m-auto flex w-full flex-col">
-                <div className="mt-5 flex justify-center">
+        <div className="m-auto flex flex-col min-h-screen w-[500px] items-center">
+            <div className="mt-5 flex w-full justify-between py-4">
+                <div className="flex items-center px-4">
                     <Link to="/">
                         <CachimboLogo />
                     </Link>
                 </div>
-
-                <div className="mt-5 flex justify-center">
+                <div className="flex items-center px-8">
                     <ThemeButton />
                 </div>
 
-                <div className="mt-8 flex justify-center text-5xl font-bold text-cach-l3 dark:text-cach-l2">
+
+            </div>
+            <section className="m-auto flex w-full flex-col">
+
+                <div className="flex justify-center text-5xl font-bold text-cach-l3 dark:text-cach-l2">
                     Iniciar sesión
                 </div>
                 <div className="w-full">
@@ -116,7 +119,7 @@ const Login = () => {
                                 className=" flex items-center justify-center focus:shadow-outline w-full rounded-xl bg-cach-l3 px-4 py-4 font-bold text-white focus:outline-none"
                                 type="submit"
                             >
-                                {isLoading ? <Loader dimensions_container="w-10 h-10" dimensions_loader="w-7 h-7" color="white" margin="my-2"/>: <span></span>} Iniciar Sesión
+                                {isLoading ? <Loader dimensions_container="w-10 h-10" dimensions_loader="w-7 h-7" color="white" margin="my-2" /> : <span></span>} Iniciar Sesión
                             </button>
                         </div>
                         <div className="flex justify-center font-light text-cach-l3 dark:text-cach-l2">
