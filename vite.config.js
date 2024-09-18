c6100f67e1ea45f8a85f5eadfb27c85c46c432db
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2024-09-17 20:00:25 -0500

    fix vitt config to deploy

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,23 @@ import { defineConfig, loadEnv } from "vite";
 import * as path from "path";
 import react from "@vitejs/plugin-react-swc";
 
-
-const env = loadEnv('development', process.cwd());
-const api_url = env.VITE_API_URL;
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: api_url,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     },
-    historyApiFallback: true,
-  }
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      },
+      historyApiFallback: true,
+    }
+  };
 });
