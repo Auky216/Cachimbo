b48cc60c4bc60584ff9ef65cdfeae56e8d0da19d
Fabrizzio20k <fabrizzio785@gmail.com>
2024-10-13 22:53:03 -0500

    Add api_url

--- src/components/fetchingData.jsx
+++ src/components/fetchingData.jsx
@@ -2,21 +2,24 @@ export const fetchDataCustom = async (data, url_api) => {
     try {
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
-    
+
         const raw = JSON.stringify(data);
-    
+
         const requestOptions = {
             method: "POST",
             headers: myHeaders,
             body: raw,
             redirect: "follow"
         };
-        const response = await fetch(`/api/${url_api}`, requestOptions);
+        const api_url = import.meta.env.VITE_API_URL;
+
+        const response = await fetch(`${api_url}/${url_api}`, requestOptions);
+
         const result = await response.json();
         return [result, JSON.parse(result.body), false, null];
     } catch (error) {
         console.error('Error:', error);
-        return [null, null , true, error];
+        return [null, null, true, error];
     }
 }
 
