29ceb5edf9f98b17c2fda81953cde217b2d81c70
Adrian Antonio Auqui Perez <91244347+Auky216@users.noreply.github.com>
2024-10-23 00:53:00 -0500

    base

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-const config = {
-  tabWidth: 2,
-  tailwindAttributes: ["className"],
-  plugins: ["prettier-plugin-tailwindcss"],
-  arrowParens: "avoid",
-};
-
-export default config;
